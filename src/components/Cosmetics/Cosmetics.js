4b441026e5f8876926684f5da7ba96a60954d5ba
ualiurrahat <ualiurrahat25@gmail.com>
2023-04-03 15:52:39 +0600

    initialize project

---
+++ src/components/Cosmetics/Cosmetics.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmetics = () => {
+    return (
+        <div>
+            <h2>Welcome To My Cosmetics Store</h2>
+        </div>
+    );
+};
+
+export default Cosmetics;
