ac462b6cc1d53c4cccc231d883ff5aaaf5a28e86
ualiurrahat <ualiurrahat25@gmail.com>
2023-04-03 22:44:22 +0600

    created event handler with parameters

--- src/components/Cosmetic/Cosmetic.js
+++ src/components/Cosmetic/Cosmetic.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import './Cosmetic.css'
 
 const Cosmetic = (props) => {
     // destructuring cosmetic object
     const {name,price,id} = props.cosmetic;
+    const addToCart =(id) =>{
+        console.log("item added", id);
+    }
+    const addToCartWrapper = () => addToCart(id);
     return (
-        <div>
+        <div className='product'>
             <h3>Product:{name}</h3>
             <p>ID:{id}</p>
             <p>Price:${price}</p>
+            {/* <button onClick={addToCartWrapper}>Add to Cart</button>
+             */}
+             <button onClick={() => addToCart(id)}>Add to Cart</button>
         </div>
     );
 };
