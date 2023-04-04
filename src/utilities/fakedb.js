5d898b1cc1bf0435ec89ff2ddd09cf18a9db7d63
ualiurrahat <ualiurrahat25@gmail.com>
2023-04-04 20:49:49 +0600

    stored multiple data in an object with localstorage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,23 +1,37 @@
 // function to manage cart data
 const addToDataBase = id =>{
-    // check if product with given key already exists or not
-    const quantity = localStorage.getItem(id);
-
+    // taking object to store as values in localStorage 
+    let shoppingCart;
+    // get the cart
+    const storedCart = localStorage.getItem('shopping-cart');
+    // if card is already being stored
+    if(storedCart)
+    {
+        // card is already stored
+        // assign the storedCart as properties of shoppingCart object
+        shoppingCart = JSON.parse(storedCart);
+    }
+    // in case of cart is new and still not stored
+    else
+    {
+        shoppingCart = {};
+    }
+//   add quantity 
+    const quantity = shoppingCart[id];
     // if quantity value is truthy, product exists with given id
     if(quantity)
     {
-        console.log("already exists");
         // update quantity of the product by adding up 1
-        const newQuantity = parseInt(quantity) + 1;
-        // store in the localStorage
-        localStorage.setItem(id,newQuantity);
+        const newQuantity = quantity +1;
+        shoppingCart[id] = newQuantity;
     }
     // proudct does not exist in the local storage
     else{
-        console.log("item added");
-        // add product in the storage
-        localStorage.setItem(id,1);
+      // add the object
+      shoppingCart[id] = 1;
     }
+    // add in the local storage 
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
 }
 
 export {addToDataBase};
